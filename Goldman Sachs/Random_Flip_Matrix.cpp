773be8d45d97058a88d5171c4c9cb7ebb0630bc5
Manish Kumar Singh <manish31311@gmail.com>
2024-01-02 01:31:25 +0530

    Create Random_Flip_Matrix.cpp

---
+++ Goldman Sachs/Random_Flip_Matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int row,col;
+    set<pair<int,int>>st;
+    Solution(int m, int n) {
+        row=m;
+        col=n;
+    }
+    vector<int> flip() {
+        int i=rand()%row;
+        int j=rand()%col;
+        if(st.find({i,j})==st.end()){
+            st.insert({i,j});
+            return {i,j};
+        }else return flip();
+    }
+    
+    void reset() {
+        st.clear();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(m, n);
+ * vector<int> param_1 = obj->flip();
+ * obj->reset();
+ */
