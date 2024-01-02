1019e9f41031189055fc0cf619d061e1c0af2ba1
Manish Kumar Singh <manish31311@gmail.com>
2024-01-02 15:19:45 +0530

    Create Query_Kth_Smallest_Trimmed_number.cpp

---
+++ Goldman Sachs/Query_Kth_Smallest_Trimmed_number.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int>ans;
+        for(int i=0;i<queries.size();i++){
+            vector<pair<string,int>>p;
+            for(int j=0;j<nums.size();j++){
+                int trim=nums[j].size()-queries[i][1];
+                string s=nums[j].substr(trim);
+                p.push_back({s,j});
+            }
+            sort(p.begin(),p.end());
+            ans.push_back(p[queries[i][0]-1].second);
+        }
+        return ans;
+    }
+};
