fa1ab75398817caffda34544fdb8f7fea7ba1626
Manish Kumar Singh <manish31311@gmail.com>
2024-01-04 19:42:09 +0530

    Update Find_Missing_&_Repeating.cpp

--- Goldman Sachs/Find_Missing_&_Repeating.cpp
+++ Goldman Sachs/Find_Missing_&_Repeating.cpp
@@ -1,21 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Solution{
+class Solution
+{
 public:
-    vector<int> findTwoElement(vector<int> arr, int n) {
-        // code here
-        map<int,int>mp;
-        for(auto i:arr) mp[i]++;
-        int r=0,m=0;
-        for(int i=1;i<=n;i++){
-            if(mp[i]==0){
-                m=i;
+    vector<int> findTwoElement(vector<int> arr, int n)
+    {
+        // Time Complexity:O(n+n(log n))
+        //                :O(n(log n))
+        // Space Complexity=O(n);
+        //         map<int,int>mp;
+        //         for(auto i:arr) mp[i]++;
+        //         int r=0,m=0;
+        //         for(int i=1;i<=n;i++){
+        //             if(mp[i]==0){
+        //                 m=i;
+        //             }
+        //             if(mp[i]==2){
+        //                 r=i;
+        //             }
+        //             if(r!=0 && m!=0) break;
+        //         }
+        //         return {r,m};
+        //
+        // Time Complexity:O(n+n)
+        //                 :O(n)
+        // Space Complexity=O(1);
+        sort(arr.begin(), arr.end());
+        int missing = 0, repeating = 0;
+        int cnt = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && arr[i] == arr[i - 1] && repeating == 0)
+            {
+                cnt--;
+                repeating = arr[i];
             }
-            if(mp[i]==2){
-                r=i;
+            if (missing == 0 && arr[i] != cnt)
+            {
+                missing = cnt;
+                cnt--;
             }
-            if(r!=0 && m!=0) break;
+            if (repeating != 0 && missing != 0)
+                break;
+            cnt++;
         }
-        return {r,m};
+        if (missing == 0)
+            missing = n;
+        return {repeating, missing};
     }
 };
