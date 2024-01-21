e553daff42cdfbc454ad6a8b0a326dc46070ee90
Manish Kumar Singh <manish31311@gmail.com>
2024-01-22 01:57:56 +0530

    Create kth-largest-element-in-a-stream.cpp

---
+++ Atlassian/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+private:
+    priority_queue<int>pq;
+    int k;
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k=k;
+        for(auto i:nums) pq.push(-i);
+    }
+    
+    int add(int val) {
+        pq.push(-val);
+        while(pq.size()>k) pq.pop();
+        return -pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
